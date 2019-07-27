e81cb0a5202d159860979a5117f3e4bfe5738e1e
LidorSho <LidorSho>
2019-07-27 13:32:22 +0300

    fixed DB

--- DbScripts/InsertArtists.sql
+++ DbScripts/InsertArtists.sql
@@ -3,6 +3,16 @@ GO
 
 INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
      VALUES (1,'Ed Sheeran',28,'England','English','~/Images/EdSheeran.jpg');
+INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
+     VALUES (2,'Dr. Dre',54,'USA','English','~/Images/DRDre.jpg');
+INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
+     VALUES (3,'Rihanna',31,'USA','English','~/Images/Rihanna.jpg');
+INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
+     VALUES (4,'Snoop Dogg',47,'USA','English','~/Images/SnoopDogg.jpg');
+INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
+     VALUES (5,'Eminem',28,'USA','English','~/Images/Eminem.jpg');
+INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
+     VALUES (6,'Katy Perry',34,'England','English','~/Images/KatyPerry.jpg');
 GO
 
 
