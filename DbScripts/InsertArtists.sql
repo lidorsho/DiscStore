1a8be27280b2d3b8d7506c70f629431cc4b4aa37
LidorSho <LidorSho>
2019-08-01 21:07:49 +0300

    fixed DB and layout

--- DbScripts/InsertArtists.sql
+++ DbScripts/InsertArtists.sql
@@ -1,18 +1,18 @@
 USE [DiscStoreDB]
 GO
 
-INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
-     VALUES (1,'Ed Sheeran',28,'England','English','~/Images/EdSheeran.jpg');
-INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
-     VALUES (2,'Dr. Dre',54,'USA','English','~/Images/DRDre.jpg');
-INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
-     VALUES (3,'Rihanna',31,'USA','English','~/Images/Rihanna.jpg');
-INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
-     VALUES (4,'Snoop Dogg',47,'USA','English','~/Images/SnoopDogg.jpg');
-INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
-     VALUES (5,'Eminem',28,'USA','English','~/Images/Eminem.jpg');
-INSERT INTO [dbo].[Artists] ([ArtistID],[Name],[Age],[Location],[Language],[ImgPath])
-     VALUES (6,'Katy Perry',34,'England','English','~/Images/KatyPerry.jpg');
+INSERT INTO [dbo].[Artists] ([Name],[Age],[Location],[Language],[ImgPath])
+     VALUES ('Ed Sheeran',28,'England','English','~/Images/EdSheeran.jpg');
+INSERT INTO [dbo].[Artists] ([Name],[Age],[Location],[Language],[ImgPath])
+     VALUES ('Dr. Dre',54,'USA','English','~/Images/DRDre.jpg');
+INSERT INTO [dbo].[Artists] ([Name],[Age],[Location],[Language],[ImgPath])
+     VALUES ('Rihanna',31,'USA','English','~/Images/Rihanna.jpg');
+INSERT INTO [dbo].[Artists] ([Name],[Age],[Location],[Language],[ImgPath])
+     VALUES ('Snoop Dogg',47,'USA','English','~/Images/SnoopDogg.jpg');
+INSERT INTO [dbo].[Artists] ([Name],[Age],[Location],[Language],[ImgPath])
+     VALUES ('Eminem',28,'USA','English','~/Images/Eminem.jpg');
+INSERT INTO [dbo].[Artists] ([Name],[Age],[Location],[Language],[ImgPath])
+     VALUES ('Katy Perry',34,'England','English','~/Images/KatyPerry.jpg');
 GO
 
 
