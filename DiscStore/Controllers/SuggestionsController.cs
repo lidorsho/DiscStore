0027df1d88d04b010466aec3bfc16f5fb3e39baf
LidorSho <LidorSho>
2019-08-07 18:54:57 +0300

    added Suggestions

---
+++ DiscStore/Controllers/SuggestionsController.cs
@@ -0,0 +1,19 @@
+﻿using DiscStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiscStore.Controllers
+{
+    public class SuggestionsController : Controller
+    {
+        private DiscStoreContext db = new DiscStoreContext();
+        // GET: Suggestions
+        public ActionResult Index()
+        {
+            return View(db.Discs.ToList());
+        }
+    }
+}
